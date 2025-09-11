25384988a2438a55415f1953b559db424085cfa4
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-09-12 00:46:23 +0530

    nav

--- src/components/bars/NavBar.jsx
+++ src/components/bars/NavBar.jsx
@@ -4,12 +4,15 @@ import Profile from '../navBar/Profile'
 export default function NavBar() {
   return (
     <>
-   <nav className='flex gap-8 mx-auto max-w-6xl py-2 '>
-    <div>MediScript</div>
-    <div className='inline-flex items-center gap-2 bg-blue-100 rounded w-auto'>
+   <nav className='flex gap-8 mx-auto max-w-6xl py-4 items-center  '>
+    <div><h1 className='text-2xl '>MediScript</h1></div>
+    <div className='inline-flex items-center gap-2 bg-blue-100 rounded w-auto px-2'>
       <img src="src\assets\search.png" alt="" className='w-4 h-4 cursor-pointer'/>
       <input type="text"  placeholder='Search Medition ,Brand,and More' className='outline-0 w-xl '></input></div>
-    <div>Cart</div>
+    <div className='inline-flex gap-2'>
+      <img src="src\assets\shopping-cart.png"  className="w-6 h-6" alt=""/>
+    <h5>Cart</h5>
+    </div>
     <Profile/>
     
     </nav>
