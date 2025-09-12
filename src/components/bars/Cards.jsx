2763c1c08f6289473ec8e1ce0d935a64a3bdd135
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-09-12 22:39:31 +0530

    category

--- src/components/bars/Cards.jsx
+++ src/components/bars/Cards.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Cards({item,index}) {
+export default function Cards({item,index,image}) {
   return (
     <div>
-        <div className='' > 
-            <img src="" alt={`${index}`} />
-            <h1 className='text-wrap'>{item}</h1>
+        <div className='grid '> 
+            <img className='w-8 h-8' src={item.image} alt="img" />
+            <h1 className='text-wrap'>{item.name}</h1>
         </div>
         
         </div>
--- src/components/bars/Category.jsx
+++ src/components/bars/Category.jsx
@@ -4,14 +4,40 @@ import Cards from './Cards'
 export default function Category() {
 
     const medicineCategory=[
-  "Analgesics",
-  "Antipyretics",
-  "Antibiotics",
-  "Antiseptics & Disinfectants",
-  "Antacids & Antiulcer Drugs",
-  "Antihistamines",
-  "Cough & Cold Medicines",
-  "Vitamins & Minerals",
+  {
+    name: "Analgesics",
+    image: "https://cdn-icons-png.flaticon.com/512/2966/2966485.png"
+  },
+  {
+    name: "Antipyretics",
+    image: "https://cdn-icons-png.flaticon.com/512/2966/2966480.png"
+  },
+  {
+    name: "Antibiotics",
+    image: "https://cdn-icons-png.flaticon.com/512/2966/2966512.png"
+  },
+  {
+    name: "Antiseptics & Disinfectants",
+    image: "https://cdn-icons-png.flaticon.com/512/2966/2966525.png"
+  },
+  {
+    name: "Antacids & Antiulcer Drugs",
+    image: "https://cdn-icons-png.flaticon.com/512/2966/2966505.png"
+  },
+  {
+    name: "Antihistamines",
+    image: "https://cdn-icons-png.flaticon.com/512/2966/2966518.png"
+  },
+  {
+    name: "Cough & Cold Medicines",
+    image: "https://cdn-icons-png.flaticon.com/512/2966/2966492.png"
+  },
+  {
+    name: "Vitamins & Minerals",
+    image: "https://cdn-icons-png.flaticon.com/512/2966/2966497.png"
+  }
+
+
  
 ]
 
@@ -19,10 +45,18 @@ export default function Category() {
   return (
     <div className='bg-white mt-2 m-4 h-auto p-4 shadow-2xl'>
         <div className=''>
-            {medicineCategory.map((item,index)=>(
-              <div className='inline-flex bg-red-50 border-2 mx-2 my-2 px-2 w-60 justify-center'>
-                <Cards  key={index} item={item} index={index+1}/>
-            </div>))}
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+  {medicineCategory.map((item, i) => (
+    <div 
+      key={i} 
+      className="flex flex-col items-center p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition"
+    >
+      <img className="w-12 h-12 mb-2" src={item.image} alt={item.name} />
+      <h1 className="text-center text-sm font-medium">{item.name}</h1>
+    </div>
+  ))}
+</div>
+
         </div>
     </div>
   )
